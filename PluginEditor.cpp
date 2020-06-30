ee8296d2a4b892817fa8d5a60384280796e38340
System Administrator <root@Randyui-MacBookPro.local>
2020-06-30 10:51:47 +0900

    first commit

---
+++ PluginEditor.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+MeterExampleAudioProcessorEditor::MeterExampleAudioProcessorEditor (MeterExampleAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    lnf.setColour (foleys::LevelMeter::lmMeterGradientLowColour, juce::Colours::green);
+
+    meter.setLookAndFeel (&lnf);
+    meter.setMeterSource (&processor.getMeterSource());
+    
+    addAndMakeVisible (meter);
+    
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (800, 800);
+}
+
+MeterExampleAudioProcessorEditor::~MeterExampleAudioProcessorEditor()
+{
+    meter.setLookAndFeel (nullptr);
+}
+
+//==============================================================================
+void MeterExampleAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    //g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    //g.setColour (Colours::white);
+    //g.setFont (15.0f);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    
+    const Rectangle<float> plotFrame (0.0f, 0.0f, getWidth(), getHeight());
+    g.setColour (Colours::lightgreen);
+    g.fillRect (plotFrame);
+    
+    
+}
+
+void MeterExampleAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor...
+    meter.setBounds(100,100,50,(getHeight()/8)*6);
+}
---
+++ PluginEditor.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class MeterExampleAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    MeterExampleAudioProcessorEditor (MeterExampleAudioProcessor&);
+    ~MeterExampleAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    MeterExampleAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MeterExampleAudioProcessorEditor)
+    foleys::LevelMeterLookAndFeel lnf;
+    foleys::LevelMeter meter { foleys::LevelMeter::HasBorder }; // See foleys::LevelMeter::MeterFlags for options
+    
+
+};
